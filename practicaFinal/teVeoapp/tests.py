2d086b186d9f84e7e457b2c6bc3aafc7daa13bbe
aMonteSl <a.montesl.2020@alumnos.urjc.es>
2024-05-03 11:41:22 +0200

    Test3

--- practicaFinal/teVeoapp/tests.py
+++ practicaFinal/teVeoapp/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase, RequestFactory
 # RequestFactory se usa para crear request falsos
 from . import manageUser
+from . import manageOrder
+from .models import Camera, Comment
 from . import views
 
 
@@ -94,3 +96,41 @@ class ManageUserTest(TestCase):
             ('TestUser',
              'font-size-pequena',
              'font-family-courier'))
+
+class ManageOrderTest(TestCase):
+    def setUp(self):
+        self.camera1 = Camera.objects.create(source_id='1', src='src1', img_path='path1')
+        self.camera2 = Camera.objects.create(source_id='2', src='src2', img_path='path2')
+        self.comment1 = Comment.objects.create(camera=self.camera1, text='comment1')
+        self.comment2 = Comment.objects.create(camera=self.camera1, text='comment2')
+        self.comment3 = Comment.objects.create(camera=self.camera2, text='comment3')
+
+    def test_order_cameras_by_comments(self):
+        cameras = manageOrder.order_cameras_by_comments('asc')
+        self.assertEqual(cameras[0], self.camera2)
+        self.assertEqual(cameras[1], self.camera1)
+
+        cameras = manageOrder.order_cameras_by_comments('desc')
+        self.assertEqual(cameras[0], self.camera1)
+        self.assertEqual(cameras[1], self.camera2)
+
+    def test_order_cameras_by_time(self):
+        cameras = manageOrder.order_cameras_by_time('asc')
+        self.assertEqual(cameras[0], self.camera1)
+        self.assertEqual(cameras[1], self.camera2)
+
+        cameras = manageOrder.order_cameras_by_time('desc')
+        self.assertEqual(cameras[0], self.camera2)
+        self.assertEqual(cameras[1], self.camera1)
+
+    def test_order_comments_by_time(self):
+        comments = Comment.objects.all()
+        ordered_comments = manageOrder.order_comments_by_time(comments, 'asc')
+        self.assertEqual(ordered_comments[0], self.comment1)
+        self.assertEqual(ordered_comments[1], self.comment2)
+        self.assertEqual(ordered_comments[2], self.comment3)
+
+        ordered_comments = manageOrder.order_comments_by_time(comments, 'desc')
+        self.assertEqual(ordered_comments[0], self.comment3)
+        self.assertEqual(ordered_comments[1], self.comment2)
+        self.assertEqual(ordered_comments[2], self.comment1)
